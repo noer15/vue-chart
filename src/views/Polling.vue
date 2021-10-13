687d8ccecfd467104cc964ac571d73840fd12d6c
Nur Hidayat <noer4236@gmail.com>
2021-10-14 01:20:26 +0700

    update new reload

--- src/views/Polling.vue
+++ src/views/Polling.vue
@@ -3,106 +3,102 @@
     <Navbar />
     <div class="lg:max-w-6xl mx-auto lg:mt-20 mt-10">
       <div class="mt-5 md:mt-0 md:col-span-2">
-        <form @submit="handleSubmit">
-          <div
-            class="overflow-hidden sm:rounded-md border shadow rounded-md p-5"
-          >
-            <div class="px-4 py-5 bg-white sm:p-6">
-              <div class="grid grid-cols-6 gap-6">
-                <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="first-name"
-                    class="block text-sm font-medium text-gray-700"
-                    >Nama Lengkap</label
-                  >
-                  <input
-                    type="text"
-                    v-model="name"
-                    placeholder="Nama Lengkap"
-                    class="mt-1 p-4 border focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
+        <div class="overflow-hidden sm:rounded-md border shadow rounded-md p-5">
+          <div class="px-4 py-5 bg-white sm:p-6">
+            <div class="grid grid-cols-6 gap-6">
+              <div class="col-span-6 sm:col-span-3">
+                <label
+                  for="first-name"
+                  class="block text-sm font-medium text-gray-700"
+                  >Nama Lengkap</label
+                >
+                <input
+                  type="text"
+                  v-model="name"
+                  placeholder="Nama Lengkap"
+                  class="mt-1 p-4 border focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
 
-                <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="last-name"
-                    class="block text-sm font-medium text-gray-700"
-                    >Nama Perusahaan</label
+              <div class="col-span-6 sm:col-span-3">
+                <label
+                  for="last-name"
+                  class="block text-sm font-medium text-gray-700"
+                  >Nama Perusahaan</label
+                >
+                <input
+                  type="text"
+                  v-model="company"
+                  placeholder="PT..."
+                  class="mt-1 p-4 border focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div class="col-span-6 sm:col-span-3">
+                <label
+                  for="state"
+                  class="block text-sm font-medium text-gray-700"
+                  >No Telepon</label
+                >
+                <input
+                  type="text"
+                  v-model="phone"
+                  placeholder="081 xxx xxx"
+                  class="mt-1 border p-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div class="col-span-6 sm:col-span-3">
+                <label
+                  for="country"
+                  class="block text-sm font-medium text-gray-700"
+                  >Kandidat</label
+                >
+                <select
+                  id="country"
+                  v-model="name_candidate"
+                  placeholder="Pilih"
+                  class="mt-1 p-4 border block w-full px-3 border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                >
+                  <option value="">Pilih Kandidat</option>
+                  <option
+                    v-for="(item, index) in candidate"
+                    :key="index"
+                    :value="item._id"
+                    >{{ item.name }}</option
                   >
-                  <input
-                    type="text"
-                    v-model="company"
-                    placeholder="PT..."
-                    class="mt-1 p-4 border focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-                <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="state"
-                    class="block text-sm font-medium text-gray-700"
-                    >No Telepon</label
-                  >
-                  <input
-                    type="text"
-                    v-model="phone"
-                    placeholder="081 xxx xxx"
-                    class="mt-1 border p-4 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                  />
-                </div>
-                <div class="col-span-6 sm:col-span-3">
-                  <label
-                    for="country"
-                    class="block text-sm font-medium text-gray-700"
-                    >Kandidat</label
-                  >
-                  <select
-                    id="country"
-                    v-model="name_candidate"
-                    placeholder="Pilih"
-                    class="mt-1 p-4 border block w-full px-3 border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  >
-                    <option value="">Pilih Kandidat</option>
-                    <option
-                      v-for="(item, index) in candidate"
-                      :key="index"
-                      :value="item._id"
-                      >{{ item.name }}</option
-                    >
-                  </select>
-                </div>
+                </select>
               </div>
             </div>
-            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-              <button
-                type="submit"
-                class="inline-flex justify-center w-full py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                <svg
-                  v-show="loading"
-                  class="animate-spin -ml-1 mr-3 h-4 w-4 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                  ></circle>
-                  <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                Simpan Polling
-              </button>
-            </div>
           </div>
-        </form>
+          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+            <button
+              @click="handleSubmit"
+              class="inline-flex justify-center w-full py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              <svg
+                v-show="loading"
+                class="animate-spin -ml-1 mr-3 h-4 w-4 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              Simpan Polling
+            </button>
+          </div>
+        </div>
 
         <!-- notif -->
         <transition
